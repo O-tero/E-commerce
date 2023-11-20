0b83b45adc11a096043296f8bb79c0d5dd0c99d0
Otero <oteronickbrian@gmail.com>
2023-11-20 12:53:42 +0300

    custom django slug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "ecommerce.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ['templates'],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -119,10 +119,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR /"static"
+STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [
-    'ecommerce/static',
+    "ecommerce/static",
 ]
+
+# Media files configuration
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
