6b74fcbd0f1098268b3f41f6479bdaa4a35121e6
Otero <oteronickbrian@gmail.com>
2023-11-20 12:33:21 +0300

    custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+# Customer user model
 
 class MyAccountManager(BaseUserManager):
     def create_user(self, first_name, last_name, username, email, password=None):
