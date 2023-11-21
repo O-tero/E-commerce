78aa48e7588081e74472984cfb8049e044ad5ed9
Otero <oteronickbrian@gmail.com>
2023-11-21 14:51:29 +0300

    single product view

--- store/views.py
+++ store/views.py
@@ -7,7 +7,7 @@ from category.models import Category
 def store(request, category_slug=None):
     categories = None
     produts = None
-    
+
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
@@ -20,3 +20,18 @@ def store(request, category_slug=None):
         "product_count": products.count,
     }
     return render(request, "store/store.html", context)
+
+
+def product_detail(request, category_slug, product_slug):
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Product.DoesNotExist:
+        # Updated the exception to Product.DoesNotExist
+        raise get_object_or_404(
+            Product, category__slug=category_slug, slug=product_slug
+        )
+
+    context = {
+        "single_product": single_product,
+    }
+    return render(request, "store/product_detail.html", context)
