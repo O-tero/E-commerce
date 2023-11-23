a838f83ec915a653d7694dce878c1cfe26424ca1
Otero <oteronickbrian@gmail.com>
2023-11-23 23:21:50 +0300

    single product url

--- store/views.py
+++ store/views.py
@@ -24,7 +24,9 @@ def store(request, category_slug=None):
 
 def product_detail(request, category_slug, product_slug):
     try:
-        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+        single_product = Product.objects.get(
+            category__slug=category_slug, slug=product_slug
+        )
     except Product.DoesNotExist:
         # Updated the exception to Product.DoesNotExist
         raise get_object_or_404(
