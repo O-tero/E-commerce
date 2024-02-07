a80af1f2a5c88e19d42135d813be549d86c85d78
Otero <oteronickbrian@gmail.com>
2024-02-07 12:55:22 +0300

    Move cart items to order product table

--- orders/views.py
+++ orders/views.py
@@ -25,11 +25,31 @@ def payments(request):
     )
 
     payment.save()
-    
+
     order.payment = payment
     order.is_ordered = True
     order.save()
-
+    
+    # Move the cart items to Order Product table
+    cart_items = CartItem.objects.filter(user=request.user)
+    
+    for item in cart_items:
+        orderproduct = OrderProduct()
+        orderproduct.order_id = order.id
+        orderproduct.payment = payment
+        orderproduct.user_id = request.user.id
+        orderproduct.product_id = item.product_id
+        orderproduct.quantity = item.quantity
+        orderproduct.product_price = item.product.price
+        orderproduct.ordered = True
+        orderproduct.save()
+        
+        cart_item = CartItem.objects.get(id=item.id)
+        product_variation = cart_item.variations.all()
+        orderproduct = OrderProduct.objects.get(id=orderproduct.id)
+        orderproduct.variations.set(product_variation)
+        orderproduct.save()
+        
     return render(request, "orders/payments.html")
 
 
