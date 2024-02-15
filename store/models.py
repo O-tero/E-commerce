14c5ca8414ba61feafa7c9d05529b5b08fb1c5f6
Otero <oteronickbrian@gmail.com>
2024-02-15 12:46:22 +0300

    Product gallery model

--- store/models.py
+++ store/models.py
@@ -4,6 +4,7 @@ from accounts.models import Account
 from django.urls import reverse
 from django.db.models import Avg, Count
 
+
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
@@ -24,18 +25,23 @@ class Product(models.Model):
         return self.product_name
 
     def averageReview(self):
-        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(
+            average=Avg("rating")
+        )
         avg = 0
-        if reviews['average'] is not None:
-            avg = float(reviews['average'])
+        if reviews["average"] is not None:
+            avg = float(reviews["average"])
         return avg
-    
+
     def countReview(self):
-        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(
+            count=Count("id")
+        )
         count = 0
-        if reviews['count'] is not None:
-            count = int(reviews['count'])
+        if reviews["count"] is not None:
+            count = int(reviews["count"])
         return count
+
 
 class VariationManager(models.Manager):
     def colors(self):
@@ -80,6 +86,18 @@ class ReviewRating(models.Model):
     status = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
         return self.subject
+
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="store/products", max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = "productgallery"
+        verbose_name_plural = "product gallery"
