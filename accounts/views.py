9aef5c18dfe7fe3eb97ce658e0f56f670251815a
Otero <oteronickbrian@gmail.com>
2024-02-15 12:13:35 +0300

    order detail page

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import RegistrationForm, UserForm, UserProfileForm
 from .models import Account, UserProfile
-from orders.models import Order
+from orders.models import Order, OrderProduct
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
@@ -134,8 +134,10 @@ def dashboard(request):
     )
     orders_count = orders.count()
 
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
     context = {
         "orders_count": orders_count,
+        "userprofile": userprofile,
     }
     return render(request, "accounts/dashboard.html", context)
 
@@ -266,3 +268,19 @@ def change_password(request):
             messages.error(request, "Password does not match!")
             return redirect("change_password")
     return render(request, "accounts/change_password.html")
+
+
+@login_required(login_url="login")
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product_price * i.quantity
+
+    context = {
+        "order_detail": order_detail,
+        "order": order,
+        "subtotal": subtotal,
+    }
+    return render(request, "accounts/order_detail.html", context)
