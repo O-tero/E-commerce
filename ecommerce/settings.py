87bc6d72aef91b2352cf0eca440edc6ea2c23c81
Otero <oteronickbrian@gmail.com>
2024-02-17 15:21:09 +0300

    Automatically logout after Inactivity

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "store",
     "carts",
     "orders",
+    "admin-honeypot",
 ]
 
 MIDDLEWARE = [
@@ -84,24 +85,16 @@ AUTH_USER_MODEL = "accounts.Account"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 # Database Configuration
-if "RDS_DB_NAME" in os.environ:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.mysql",
-            "NAME": os.environ["RDS_DB_NAME"],
-            "USER": os.environ["RDS_USERNAME"],
-            "PASSWORD": os.environ["RDS_PASSWORD"],
-            "HOST": os.environ["RDS_HOSTNAME"],
-            "PORT": os.environ["RDS_PORT"],
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "ecommerce",
+        "USER": "root",
+        "PASSWORD": "Nick334$",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
